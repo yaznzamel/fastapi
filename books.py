92ce5ed4f0bb76fd5ab1cbd46ab34dd3740db0b1
yaznzamel <yaznzamel2@gmail.com>
2023-08-08 21:57:32 +0300

    init

---
+++ books.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI,Body
+
+
+app= FastAPI()
+
+
+books=[
+    {'title': 'Cloud Computing','author':'Yazn Zamel','Category':'science'},
+    {'title': ' Computing','author':' Zamel','Category':'IT'}
+]
+
+@app.get("/book")
+async def first_api():
+    return books
+
+
+#static path parameter
+@app.get("/book/book1")
+async def sec():
+    return books[1]
+
+#dynamic path parameters
+@app.get("/book/{dp}")
+async def dp(dp):
+    for i in books:
+        if i['title']==dp:
+            return i
+
+
+@app.get("/allbooks")
+async def all():
+    return books
+
+
+
+# POST Requests
+
+
+@app.post("/create_book")
+async def create_book(new_book=Body()):
+    books.append(new_book) 
